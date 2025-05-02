645ddd945103f8f71b5364c4a75e441668c7b8e2
jeffIshmael <jeffishmael141@gmail.com>
2025-05-02 22:50:00 +0300

    update: changing mpesa callback api

--- packages/frontend/app/api/mpesa-callback/route.ts
+++ packages/frontend/app/api/mpesa-callback/route.ts
@@ -1,30 +1,28 @@
 // app/api/mpesa-callback/route.ts
 import { NextResponse } from 'next/server';
-import { headers } from 'next/headers';
-import { sendEmail } from '@/app/actions/emailService';
+
 
 export async function POST(request: Request) {
+  const { headers } = await import('next/headers');
+  const { sendEmail } = await import('@/app/actions/emailService');
+  
   const headersList = headers();
   console.log("headersList", headersList);
-//   const signature = headersList.get('x-mpesa-signature');
   
-  // Verify signature (compare with your generated signature)
   try {
     const body = await request.json();
     if (body.Body.stkCallback.ResultCode === 0) {
       await sendEmail("Confirmation text", JSON.stringify(body, null, 2));
       console.log("Payment successful", body);
     } else {
-      await sendEmail("cancelled text", JSON.stringify(body,null,2));
+      await sendEmail("cancelled text", JSON.stringify(body, null, 2));
       console.log("Payment failed", body);
-
     }
     return NextResponse.json({ success: true });
   } catch (err) {
     console.error("Error handling MPesa callback:", err);
     return NextResponse.json({ success: false, error: 'Internal error' }, { status: 500 });
   }
-  
 }
 
 export const dynamic = 'force-dynamic'; // Required for external callbacks
