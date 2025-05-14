4621a5af3d1d89501ae68b62e70a9aaf0f71009a
jeffIshmael <jeffishmael141@gmail.com>
2025-05-14 23:14:38 +0300

    differntiating the farcaster client

--- packages/frontend/lib/farcasterUser.ts
+++ packages/frontend/lib/farcasterUser.ts
@@ -1,6 +1,20 @@
 // this file handles getting farcaster user details from address i.e fid, username
 "use server";
-import farcasterClient from "./neynarClient";
+import { NeynarAPIClient, Configuration } from "@neynar/nodejs-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Make sure to set NEYNAR_API_KEY in .env
+// Get an API key at neynar.com
+if (!process.env.NEYNAR_API_KEY) {
+  throw new Error("NEYNAR_API_KEY not found in .env");
+}
+
+const config = new Configuration({
+  apiKey: process.env.NEYNAR_API_KEY,
+});
+
+const farcasterClient = new NeynarAPIClient(config);
 
 let userDetails: {
   fid: number;
